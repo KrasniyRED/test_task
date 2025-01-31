7b2b6403a8964d8cdbb9c720e6b4a44711a2da48
Ivan Bedov <freginder@mail.ru>
2025-01-31 17:37:27 +0700

    Создал формы и представления для CRUD операций с новостями

--- DjangoApp/news/views.py
+++ DjangoApp/news/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import News
+from .forms import NewsForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def news_list(request):
+    news = News.objects.all()
+    return render(request, 'news/news_list.html', {'news': news})
+
+@login_required
+def news_detail(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    return render(request, 'news/news_detail.html', {'news': news})
+
+@login_required
+def news_create(request):
+    if request.method == "POST":
+        form = NewsForm(request.POST, request.FILES)
+        if form.is_valid():
+            news = form.save(commit=False)
+            news.author = request.user
+            news.save()
+            return redirect('news_detail', pk=news.pk)
+    else:
+        form = NewsForm()
+    return render(request, 'news/news_edit.html', {'form': form})
+
+@login_required
+def news_edit(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    if request.method == "POST":
+        form = NewsForm(request.POST, request.FILES, instance=news)
+        if form.is_valid():
+            news = form.save(commit=False)
+            news.author = request.user
+            news.save()
+            return redirect('news_detail', pk=news.pk)
+    else:
+        form = NewsForm(instance=news)
+    return render(request, 'news/news_edit.html', {'form': form})
+
+@login_required
+def news_delete(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    news.delete()
+    return redirect('news_list')
