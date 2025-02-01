0b8955a05b87bfb1b5748ac248f01850389a9ae8
Ivan Bedov <freginder@mail.ru>
2025-02-01 19:43:53 +0700

    Интеграция summernote в админ-панель django

--- DjangoApp/news/admin.py
+++ DjangoApp/news/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import News
 
 @admin.register(News)
-class NewsAdmin(admin.ModelAdmin):
+class NewsAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'publication_date')
     list_filter = ('publication_date', 'author')
     search_fields = ('title', 'content')
     date_hierarchy = 'publication_date'
+    summernote_fields = ('content',)
 
     def get_fields(self, request, obj=None):
         fields = super().get_fields(request, obj)
