c756b08d4bb5541d75fd55b5c9fb932964cad525
Ivan Bedov <freginder@mail.ru>
2025-02-03 08:21:45 +0700

    Настройка gitignore, исправление форматирования текста, созданного в summernote

--- DjangoApp/news/urls.py
+++ DjangoApp/news/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-   
+    path('<int:pk>/', views.news_detail, name='news_detail'),
 ]
--- DjangoApp/news/views.py
+++ DjangoApp/news/views.py
@@ -4,3 +4,7 @@ from .models import News
 def home(request):
     news = News.objects.all().order_by('-publication_date')
     return render(request, 'news/home.html', {'news': news})
+
+def news_detail(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    return render(request, 'news/news_detail.html', {'news': news})
