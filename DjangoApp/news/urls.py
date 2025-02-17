6ddd8ed020169eec1c4fc265f2901db17c906165
Ivan Bedov <practiceba@cifra-k.local>
2025-02-17 14:11:48 +0700

    Отказ от ViewSets

--- DjangoApp/news/urls.py
+++ DjangoApp/news/urls.py
@@ -1,19 +1,9 @@
+from django.urls import path
 from . import views
-from rest_framework import routers
 
-router = routers.DefaultRouter(trailing_slash = False)
-router.register(r'news', views.GenericNewsViewSet, 'news')
-
-# urlpatterns = [
-#     path('', views.home, name='home'),
-#     path('<int:pk>/', views.news_detail, name='news_detail'),
-#     path('news',views.GenericNewsViewSet.as_view({'get': 'get_all'})),
-#     path('news', views.GenericNewsViewSet.as_view({'post': 'post_news'})),
-#     path('news/<int:pk>', views.GenericNewsViewSet.as_view({'get': 'get_one'})),
-#     path('news/<int:pk>', views.GenericNewsViewSet.as_view({'patch': 'update_news'})),
-#     path('news/<int:pk>', views.GenericNewsViewSet.as_view({'delete': 'delete_news'})),
-    
-# ]
-
-urlpatterns = router.urls
-
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<int:pk>/', views.news_detail, name='news_detail'),
+    path('news', views.GenericNewsView.as_view()),
+    path('news/<int:pk>', views.GenericDetailNewsView.as_view()),
+]
--- DjangoApp/news/views.py
+++ DjangoApp/news/views.py
@@ -3,40 +3,41 @@ from .models import News
 from .serializers import NewsSerializer
 from django.http import HttpRequest, HttpResponse
 from rest_framework import generics, request, response, viewsets
-from rest_framework.decorators import action
 
 class GenericNewsView(generics.GenericAPIView):
     queryset = News.objects.all().order_by('-publication_date')
     serializer_class = NewsSerializer
-    
-class GenericNewsViewSet(GenericNewsView,viewsets.ViewSet):
-    
-
-    def list(self,*args, **kwargs)-> response.Response:
+    def get(self,*args, **kwargs)-> response.Response:
         serializer = self.get_serializer(self.get_queryset(),many = True)
         return response.Response(serializer.data)
     
-    def retrieve(self,*args, **kwargs)-> response.Response:
-        serializer = self.get_serializer(self.get_object())
-        return response.Response(serializer.data)
-    
-    def create(self,request:request.Request)-> response.Response:
+    def post(self,request:request.Request)-> response.Response:
         serializer = self.get_serializer(data = request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return response.Response(status=200)
     
-    def partial_update(self,request:request.Request,*args, **kwargs)-> response.Response:
+
+class GenericDetailNewsView(GenericNewsView):
+    
+    def get(self,*args, **kwargs)-> response.Response:
+        serializer = self.get_serializer(self.get_object())
+        return response.Response(serializer.data)
+    
+    def patch(self,request:request.Request,*args, **kwargs)-> response.Response:
         instance = self.get_object()
         serializer = self.get_serializer(instance, data = request.data)
         serializer.is_valid(raise_exception = True)
         serializer.save()
         return response.Response(serializer.data)
     
-    def destroy(self,request:request.Request,*args, **kwargs)-> response.Response:
+    def delete(self,request:request.Request,*args, **kwargs)-> response.Response:
         instance : News = self.get_object()
         instance.delete()
         return response.Response(status=204)
+
+
+
 
 
 def home(request: HttpRequest) -> HttpResponse:
